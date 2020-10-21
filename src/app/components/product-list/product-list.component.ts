caf5ddb61161215aa93636e2467bdc322c726470
Jason Pis-an <jpisan@Jasons-MacBook-Air.local>
2020-10-22 02:09:16 +0800

    Update Angular component

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  //Setup a property, an array of product
+  products: Product[];
 
+  //Inject our ProductService dependency here in product-list component
+  constructor(private productService: ProductService) { }
+
+  /*
+  once an angular component is initialized
+  they'll call the ngOnit method this is similar to PostConstruct
+  */
   ngOnInit(): void {
+    //This is where i'll add the hook to call my list products method
+    this.listProducts();
+  }
+
+  listProducts() {
+    /*
+    inside use productService and call productList and subscribe to that data
+    and then the method is actually invoked once subscribe
+    */
+    this.productService.getProductList().subscribe(
+      //so this method will execute in an asynchronous fashion
+      data => {
+        //when the data is return we can assign it to our own property.
+        //So in heresimply assign the result to our product Array.
+        this.products = data;
+      }
+    )
   }
 
 }
