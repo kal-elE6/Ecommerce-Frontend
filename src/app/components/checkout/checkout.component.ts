e70161b89b0466341456fbf811a54b5b3f1f1012
Jason Pis-an <jpisan@Jasons-MacBook-Air.local>
2020-11-03 22:10:50 +0800

    Check out form layout - shipping address

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -19,6 +19,28 @@ export class CheckoutComponent implements OnInit {
         firstname: [''],
         lastname: [''],
         email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
       })
     });
   }
